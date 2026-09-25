d6e281cd4dc45d460d2e987dffbb61afd9f64b49
Akash Annamalai <akashannamalai@Akashs-MacBook-Air.local>
2026-09-25 18:18:22 +0530

    fix: add missing AdminDashboard import to App.jsx to resolve blank screen crash

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import DoctorDashboard from './pages/DoctorDashboard.jsx';
+import AdminDashboard from './pages/AdminDashboard.jsx';
 import PatientDashboard from './pages/PatientDashboard.jsx';
 import ProfessionalOnboarding from './pages/ProfessionalOnboarding.jsx';
 import Physiotherapy from './pages/Physiotherapy.jsx';
